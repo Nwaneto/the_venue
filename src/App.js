2d39aa74a7ebb3af3bec769a863594355ec660ca
Nwaneto <nwanetochidiebere@gmail.com>
2019-09-10 14:33:22 +0100

    Finished with carrousel and Time stamp

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header-footer/Header';
 
+import Feature from './components/featured';
+
 function App() {
   return (
     <div className="App" style={{height:"1500px", background:'cornflowerblue'}}>
       <Header />
+      <Feature/>
     </div>
   );
 }
